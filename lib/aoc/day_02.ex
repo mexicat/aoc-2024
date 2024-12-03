ea254073a859c8ece91a6daa99b7252d613d4398
Giacomo Magnanini <gikappa@gmail.com>
2024-12-03 08:12:44 +0100

    format

--- lib/aoc/day_02.ex
+++ lib/aoc/day_02.ex
@@ -2,42 +2,36 @@ defmodule AoC.Day02 do
   def part1(input) do
     input
     |> parse_input()
-    |> Enum.count(&validate_report(&1, nil))
+    |> Enum.count(&validate(&1, nil))
   end
 
   def part2(input) do
     input
     |> parse_input()
     |> Enum.count(fn report ->
-      case validate_report(report, nil) do
+      case validate(report, nil) do
         true ->
           true
 
         false ->
           report
           |> Enum.with_index()
-          |> Enum.any?(fn {_x, i} -> report |> List.delete_at(i) |> validate_report(nil) end)
+          |> Enum.any?(fn {_x, i} -> report |> List.delete_at(i) |> validate(nil) end)
       end
     end)
   end
 
-  def validate_report([a, b | rest], nil) when a > b,
-    do: validate_report([a, b | rest], :decreasing)
+  def validate([a, b | rest], nil) when a > b, do: validate([a, b | rest], :decr)
+  def validate([a, b | rest], nil) when a < b, do: validate([a, b | rest], :incr)
 
-  def validate_report([a, b | rest], nil) when a < b,
-    do: validate_report([a, b | rest], :increasing)
+  def validate([a, b | rest], :decr) when a - b <= 3 and a - b >= 1,
+    do: validate([b | rest], :decr)
 
-  def validate_report([a, b | _], _) when a == b, do: false
+  def validate([a, b | rest], :incr) when b - a <= 3 and b - a >= 1,
+    do: validate([b | rest], :incr)
 
-  def validate_report([a, b | rest], :decreasing) do
-    if a - b <= 3 && a - b >= 1, do: validate_report([b | rest], :decreasing), else: false
-  end
-
-  def validate_report([a, b | rest], :increasing) do
-    if b - a <= 3 && b - a >= 1, do: validate_report([b | rest], :increasing), else: false
-  end
-
-  def validate_report([_], _), do: true
+  def validate([_, _ | _], _), do: false
+  def validate([_], _), do: true
 
   def parse_input(input) do
     input
